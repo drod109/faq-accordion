c4f755a960ee77a6cf922faadb2f1a9299c076a4
Dwayne Rodrigues <dwayne.rodrigues@hotmail.ca>
2024-01-02 15:21:19 -0500

    style update

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", fuction () {
-    var
+const allDetails = document.querySelectorAll("details");
+
+function closeAllExcept(currentDetail) {
+  allDetails.forEach((detail) => {
+    if (detail !== currentDetail) {
+      detail.removeAttribute("open");
+    }
+  });
+}
+
+allDetails.forEach((detail) => {
+  detail.querySelector("summary").addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const isOpen = detail.hasAttribute("open");
+
+    closeAllExcept(detail);
+
+    if (!isOpen) {
+      detail.setAttribute("open", "");
+    }
+  });
 });
